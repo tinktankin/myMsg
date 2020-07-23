b74bc9af2055a56a120bc6c619d033b026adecf6
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-07-23 21:07:00 +0530

    appended static folder to urlpatterns

--- EmailDD/urls.py
+++ EmailDD/urls.py
@@ -21,11 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user.urls')),
-    path('',include('Campaign.urls')),
-    path('',include('Contact_Grps.urls')),
+    path('', include('user.urls')),
+    path('', include('Campaign.urls')),
+    path('', include('Contact_Grps.urls')),
     path('accounts/', include('allauth.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-
-    ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
